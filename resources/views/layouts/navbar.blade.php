2d9fd992e5450a0d2458edf77b4ce0fd8ac52d62
WallansDev <timothe.vaquie1@gmail.com>
2026-02-23 12:02:08 +0100

    Modification du style de la navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,30 +1,41 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand d-flex align-items-center" href="{{ route('home') }}">
-            <img id="kastor-logo" src="{{ asset('images/kastor.png') }}" alt="Logo" width="60" height="60"
-                class="me-2">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: white;">
+    <div class="container-fluid px-4">
+        <a class="navbar-brand d-flex align-items-center gap-2" style="color: black;" href="{{ route('home') }}">
+            <img id="kastor-logo" src="{{ asset('images/kastor.png') }}" alt="Logo" width="60" height="60">
             {{ strtoupper(config('app.name', 'Kastor Formulaires')) }}
         </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto">
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto align-items-center">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Formulaires de création de PBX
+                    <a class="nav-link dropdown-toggle px-3 py-2 rounded" href="#" style="color: grey;"
+                        id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa fa-phone me-1"></i> Formulaires de création de PBX
                     </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="{{ route('wildix.general_info') }}"><img
-                                    src="{{ asset('images/wildix.png') }}" width="15%" /> Je souhaite créer un PBX
-                                Wildix</a></li>
-                        <li><a class="dropdown-item" href="{{ route('yeastar.general_info') }}"><img
-                                    src="{{ asset('images/yeastar.png') }}" width="15%" /> Je souhaite créer un PBX
-                                Yeastar</a></li>
+                    <ul class="dropdown-menu dropdown-menu-end shadow border-0 mt-2" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center gap-2 py-2"
+                                href="{{ route('wildix.general_info') }}">
+                                <img src="{{ asset('images/wildix.png') }}" height="20" alt="Wildix">
+                                Je souhaites créer un PBX Wildix
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider my-1">
+                        </li>
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center gap-2 py-2"
+                                href="{{ route('yeastar.general_info') }}">
+                                <img src="{{ asset('images/yeastar.png') }}" height="20" alt="Yeastar">
+                                Je souhaites créer un PBX Yeastar
+                            </a>
+                        </li>
                     </ul>
                 </li>
             </ul>
